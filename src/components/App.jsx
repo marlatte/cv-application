b7896e1ffdcb1ebc854a5953308734412fe3fcef
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-27 12:52:17 -0500

    Config linter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
+// eslint-disable-next-line import/no-unresolved, import/no-absolute-path
 import viteLogo from '/vite.svg';
 import '../styles/App.css';
 
@@ -9,16 +10,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          type="button"
+          onClick={() => setCount((newCount) => newCount + 1)}
+        >
           count is {count}
         </button>
         <p>
