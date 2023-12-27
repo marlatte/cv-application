8fcca2cef133937fa664d686f6600fd0a4447999
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-27 15:37:20 -0500

    Pass form data as props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,32 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
 
-function Form(name) {
-  const [mode, setMode] = useState(false);
+const toTitleCase = (str) =>
+  str
+    .split('-')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+
+function Form({ formData }) {
+  const { formName, inputs } = formData;
+  const [editMode, setEditMode] = useState(false);
 
   function handleClick() {
-    setMode(!mode);
+    setEditMode(!editMode);
   }
 
   return (
-    <form className={name}>
-      <Input mode={mode} type="text" id="name" />
+    <form className={formName}>
+      <h1>{toTitleCase(formName)}</h1>
+      {inputs.map(({ type, domId, init }) => (
+        <Input
+          key={domId}
+          editMode={editMode}
+          type={type}
+          domId={domId}
+          init={init}
+        />
+      ))}
       <button type="button" onClick={handleClick}>
         Change
       </button>
@@ -19,15 +35,27 @@ function Form(name) {
   );
 }
 
-function Input({ mode, type, id }) {
-  return (
-    mode && (
-      <label>
-        {id}
-        <input type={type} name={id} id={id} />
-      </label>
-    )
+function Input({ editMode, type, domId, init }) {
+  const [content, setContent] = useState(init);
+
+  const displayView = <p id={domId}>{content}</p>;
+
+  const editView = (
+    <>
+      <label htmlFor={domId} style={{ marginRight: 10 }}>{`${toTitleCase(
+        domId
+      )}:`}</label>
+      <input
+        type={type}
+        name={domId}
+        id={domId}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        autoComplete={domId}
+      />
+    </>
   );
+  return editMode ? editView : displayView;
 }
 
 export default Form;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import './App';
 import '../styles/index.css';
+import Form from './Form';
+
+const formsData = {
+  genInfo: {
+    formName: 'general-info',
+    inputs: [
+      { type: 'text', domId: 'name', init: 'John Smith' },
+      { type: 'email', domId: 'email', init: 'jsmith@example.com' },
+    ],
+  },
+  education: {
+    formName: 'education',
+    inputs: [
+      { type: 'text', domId: 'school', init: 'University of Georgia' },
+      {
+        type: 'text',
+        domId: 'degree',
+        init: 'Bachelor of Arts',
+      },
+    ],
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Form formData={formsData.genInfo} />
+    <Form formData={formsData.education} />
   </React.StrictMode>
 );
